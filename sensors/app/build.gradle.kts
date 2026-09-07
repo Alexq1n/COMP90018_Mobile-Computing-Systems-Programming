2eca2c4dbbc897c80af1e68923c59c97296ddec3
Alexq1n <141204255+Alexq1n@users.noreply.github.com>
2026-09-07 10:44:36 +1000

    Test deriving data from sensors and GPS using Eventbus

--- sensors/app/build.gradle.kts
+++ sensors/app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation("org.greenrobot:eventbus:3.3.1")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
--- sensors/app/src/main/java/com/example/sensors/MainActivity.kt
+++ sensors/app/src/main/java/com/example/sensors/MainActivity.kt
@@ -1,47 +1,142 @@
 package com.example.sensors
 
+import android.Manifest
+import androidx.activity.result.contract.ActivityResultContracts
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.sensors.ui.theme.SensorsTheme
+import androidx.compose.ui.unit.dp
+import com.example.sensors.AccelerometerMessage
+import com.example.sensors.Accelerometer
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var accelerometer: Accelerometer
+    private lateinit var lightSensor: LightSensor
+    private lateinit var locationSensor: LocationSensor
+
+    private var x by mutableFloatStateOf(0f)
+    private var y by mutableFloatStateOf(0f)
+    private var z by mutableFloatStateOf(0f)
+
+    private var light by mutableFloatStateOf(0f)
+
+    private var latitude by mutableDoubleStateOf(0.0)
+    private var longitude by mutableDoubleStateOf(0.0)
+
+
+    private val locationPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { granted ->
+
+            if (granted) {
+                locationSensor.enableLocation()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        accelerometer = Accelerometer(this)
+        lightSensor = LightSensor(this)
+        locationSensor = LocationSensor(this)
         setContent {
-            SensorsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(30.dp)
+            ) {
+                Text("RoamMate Sensors")
+
+                Text("Accelerometer")
+
+                Text("X: %.2f".format(x))
+
+                Text("Y: %.2f".format(y))
+
+                Text("Z: %.2f".format(z))
+
+                Text("Light Sensor")
+
+                Text("Light: %.2f lux".format(light))
+
+                Text("Location")
+                Text("Latitude: %.6f".format(latitude))
+                Text("Longitude: %.6f".format(longitude))
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onStart() {
+        super.onStart()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SensorsTheme {
-        Greeting("Android")
+        EventBus.getDefault().register(this)
+
+        accelerometer.enableSensor()
+        lightSensor.enableSensor()
+        locationSensor.enableLocation()
+        if (
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            locationSensor.enableLocation()
+        } else {
+            locationPermissionLauncher.launch(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        }
+    }
+
+    override fun onStop() {
+
+        EventBus.getDefault().unregister(this)
+
+        accelerometer.disableSensor()
+        lightSensor.disableSensor()
+        locationSensor.disableLocation()
+        super.onStop()
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onAccelerometerMessage(
+        message: AccelerometerMessage
+    ) {
+
+        x = message.x
+        y = message.y
+        z = message.z
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onLightSensorMessage(
+        message: LightSensorMessage
+    ) {
+
+        light = message.light
+    }
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onLocationMessage(
+        message: LocationMessage
+    ) {
+
+        latitude = message.latitude
+        longitude = message.longitude
     }
 }
